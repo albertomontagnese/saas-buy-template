248c8e6022fe422760fa22855cb9533dc471edc5
Alberto <alberto.montagnese@gmail.com>
2025-01-02 20:31:36 -0800

    initial commit

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,6 +2,28 @@ import { Button } from '@/components/ui/button';
 import { ArrowRight, CreditCard, Database } from 'lucide-react';
 import { Terminal } from './terminal';
 
+
+
+const VideoPlayer = () => {
+  const videoSrc = 'https://streamable.com/k9zsfy';
+
+  return (
+    <div>
+      <video
+        src={videoSrc}
+        autoPlay
+        loop
+        controls
+        style={{ width: '100%', maxHeight: '500px' }}
+      >
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
+
+
+
 export default function HomePage() {
   return (
     <main>
@@ -10,16 +32,14 @@ export default function HomePage() {
           <div className="lg:grid lg:grid-cols-12 lg:gap-8">
             <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
               <h1 className="text-4xl font-bold text-gray-900 tracking-tight sm:text-5xl md:text-6xl">
-                Build Your SaaS
+                Create engaging Papers for your social media
                 <span className="block text-orange-500">Faster Than Ever</span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-                Launch your SaaS product in record time with our powerful,
-                ready-to-use template. Packed with modern technologies and
-                essential integrations.
+                Papersnap is a tool that helps you create engaging papers for your social media.
               </p>
               <div className="mt-8 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
-                <a
+                {/* <a
                   href="https://vercel.com/templates/next.js/next-js-saas-starter"
                   target="_blank"
                 >
@@ -27,11 +47,12 @@ export default function HomePage() {
                     Deploy your own
                     <ArrowRight className="ml-2 h-5 w-5" />
                   </Button>
-                </a>
+                </a> */}
               </div>
             </div>
             <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
-              <Terminal />
+              {/* <Terminal /> */}
+              <VideoPlayer />
             </div>
           </div>
         </div>
@@ -98,21 +119,19 @@ export default function HomePage() {
           <div className="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
             <div>
               <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">
-                Ready to launch your SaaS?
+                Ready to create engaging papers?
               </h2>
               <p className="mt-3 max-w-3xl text-lg text-gray-500">
-                Our template provides everything you need to get your SaaS up
-                and running quickly. Don't waste time on boilerplate - focus on
-                what makes your product unique.
+                Papersnap is a tool that helps you create engaging papers for your social media.
               </p>
             </div>
             <div className="mt-8 lg:mt-0 flex justify-center lg:justify-end">
               <a
-                href="https://github.com/leerob/next-saas-starter"
+                href="/sign-up"
                 target="_blank"
               >
                 <Button className="bg-white hover:bg-gray-100 text-black border border-gray-200 rounded-full text-xl px-12 py-6 inline-flex items-center justify-center">
-                  View the code
+                  Get Started
                   <ArrowRight className="ml-3 h-6 w-6" />
                 </Button>
               </a>
